2117d8af1358e7bd124328c07a8a8b9e20de8111
Dario Simola <dariosimola@gmail.com>
2022-04-05 18:48:53 +0200

    fix SearchForm and add tests

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,75 @@
 import React from "react";
-
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const SearchForm = () => {
-  return <></>;
+  return (
+    <Formik
+      initialValues={{ username: "", subreddit: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .max(20, "Must be between 3 and 20 characters")
+          .min(3, "Must be between 3 and 20 characters")
+          .required("Required"),
+        subreddit: Yup.string()
+          .max(3, "Must be between 3 and 20 characters")
+          .min(3, "Must be between 3 and 20 characters"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {(formik) => (
+        <Form className="col-lg-2 offset-lg-5 ">
+          <div className="form-group mb-3">
+            <label htmlFor="username">Username</label>
+            <Field
+              name="username"
+              className={
+                formik.touched.username && formik.errors.username
+                  ? "form-control is-invalid"
+                  : "form-control"
+              }
+              type="text"
+              aria-label="username"
+            />
+            <ErrorMessage name="username" />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="subreddit">Subreddit</label>
+
+            <Field
+              name="subreddit"
+              className={
+                formik.touched.subreddit && formik.errors.subreddit
+                  ? "form-control is-invalid"
+                  : "form-control"
+              }
+              type="text"
+              aria-label="subreddit"
+            />
+
+            <Field as="select" name="color">
+              <option value="red">Red</option>
+
+              <option value="green">Green</option>
+
+              <option value="blue">Blue</option>
+            </Field>
+
+            <ErrorMessage name="subreddit" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 export default SearchForm;
--- src/test/SearchForm.test.js
+++ src/test/SearchForm.test.js
@@ -1,13 +1,75 @@
 import SearchForm from "../components/SearchForm";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import userEvent from "@testing-library/user-event";
 
-test("Submit blank form fails", async () => {
-  render(<SearchForm />);
+describe("Search Form", () => {
+  test("Submit blank form fails", async () => {
+    render(<SearchForm />);
 
-  userEvent.click(screen.getByRole("button", { name: /search/i }));
-  const errorMessage = await screen.findByText(/Required/i);
+    const button = screen.getByRole("button", { name: /search/i });
 
-  expect(errorMessage).toBeInTheDocument();
+    await waitFor(() => userEvent.click(button));
+
+    const errorMessage = await screen.findByText(/Required/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+  test("username with less than 3 characters fails", async () => {
+    render(<SearchForm />);
+
+    const button = screen.getByRole("button", { name: /search/i });
+    const usernameTextbox = screen.getByRole("textbox", { name: /username/i });
+    await waitFor(() => userEvent.type(usernameTextbox, "aa"));
+    await waitFor(() => userEvent.click(button));
+    const errorMessage = await screen.findByText(
+      /must be between 3 and 20 characters/i
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
+  test("subreddit with less than 3 characters fails", async () => {
+    render(<SearchForm />);
+
+    const button = screen.getByRole("button", { name: /search/i });
+    const subredditTextbox = screen.getByRole("textbox", {
+      name: /subreddit/i,
+    });
+    await waitFor(() => userEvent.type(subredditTextbox, "aa"));
+    await waitFor(() => userEvent.click(button));
+    const errorMessage = await screen.findByText(
+      /must be between 3 and 20 characters/i
+    );
+    await waitFor(() => expect(errorMessage).toBeInTheDocument());
+  });
+
+  test("username with more than 20 characters fails", async () => {
+    render(<SearchForm />);
+
+    const button = screen.getByRole("button", { name: /search/i });
+    const usernameTextbox = screen.getByRole("textbox", { name: /username/i });
+    await waitFor(() =>
+      userEvent.type(usernameTextbox, "aaaaaaaaaaaaaaaaaaaaa")
+    );
+    await waitFor(() => userEvent.click(button));
+    const errorMessage = await screen.findByText(
+      /must be between 3 and 20 characters/i
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  test("subreddit with more than 20 characters fails", async () => {
+    render(<SearchForm />);
+
+    const button = screen.getByRole("button", { name: /search/i });
+    const subredditTextbox = screen.getByRole("textbox", {
+      name: /subreddit/i,
+    });
+    await waitFor(() =>
+      userEvent.type(subredditTextbox, "aaaaaaaaaaaaaaaaaaaaa")
+    );
+    await waitFor(() => userEvent.click(button));
+    const errorMessage = await screen.findByText(
+      /must be between 3 and 20 characters/i
+    );
+    await waitFor(() => expect(errorMessage).toBeInTheDocument());
+  });
 });
