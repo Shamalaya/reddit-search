53d0662fd905b39abacbc1984aef43dc5347cdde
Dario Simola <dariosimola@gmail.com>
2022-04-15 23:01:02 +0200

    feat: add sort

---
+++ src/components/Sort.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { selectSort, setSort } from "../features/message/messagesSlice";
+
+const Sort = () => {
+  let sort = useSelector(selectSort);
+  const dispatch = useDispatch();
+
+  return (
+    <Form.Group className="col-lg-1 offset-lg-11 ">
+      <Form.Label htmlFor="sort">Sort by</Form.Label>
+      <Form.Select
+        name="sort"
+        id="sort"
+        className="w-100 p-2"
+        value={sort}
+        onChange={(e) => dispatch(setSort(e.target.value))}
+      >
+        <option value="newer">Newer</option>
+        <option value="older">Older</option>
+        <option value="popular">Popular</option>
+        <option value="unpopular">Unpopular</option>
+      </Form.Select>
+    </Form.Group>
+  );
+};
+
+export default Sort;
--- src/features/message/messagesSlice.js
+++ src/features/message/messagesSlice.js
@@ -1,10 +1,12 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getMessages } from "../../API";
-
+// eslint-disable-next-line no-unused-vars
+import dayjs from "dayjs";
 const initialState = {
   data: null,
   isLoading: false,
   isError: false,
+  sort: "date-desc",
 };
 
 export const fetchMessages = createAsyncThunk(
@@ -13,10 +15,17 @@ export const fetchMessages = createAsyncThunk(
     return getMessages(values);
   }
 );
+
 const messagesSlice = createSlice({
   name: "messages",
   initialState,
-  reducers: {},
+  reducers: {
+    setSort: (state, action) => {
+      console.log("sto qua");
+      console.log(action);
+      state.sort = action.payload;
+    },
+  },
   extraReducers: {
     [fetchMessages.pending]: (state) => {
       state.isLoading = true;
@@ -31,5 +40,35 @@ const messagesSlice = createSlice({
     },
   },
 });
+export const selectSort = (state) => {
+  return state.messages.sort;
+};
+export const selectSortedMessages = (state) => {
+  let sortedMessages = [...state.messages.data];
+
+  switch (state.messages.sort) {
+    case "newer":
+      sortedMessages.sort((a, b) =>
+        dayjs(a.created_utc).isBefore(dayjs(b.created_utc))
+      );
+      break;
+    case "older":
+      sortedMessages.sort((a, b) =>
+        dayjs(b.created_utc).isBefore(dayjs(a.created_utc))
+      );
+      break;
+    case "popular":
+      sortedMessages.sort((a, b) => b.score - a.score);
+      break;
+    case "unpopular":
+      sortedMessages.sort((a, b) => a.score - b.score);
+      break;
+    default:
+      break;
+  }
+  return sortedMessages;
+};
+
+export const { setSort } = messagesSlice.actions;
 
 export default messagesSlice.reducer;
